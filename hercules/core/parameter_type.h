b22506bf720bd5b5ba5dbd2ecc8c225f96e6b723
1552739156@qq.com <110255362+tiger631@users.noreply.github.com>
2022-10-12 06:13:10 +0800

    init

---
+++ hercules/core/parameter_type.h
@@ -0,0 +1,18 @@
+
+/****************************************************************
+ * Copyright (c) 2022, liyinbin
+ * All rights reserved.
+ * Author by liyinbin (jeff.li) lijippy@163.com
+ *****************************************************************/
+
+#ifndef HERCULES_CORE_PARAMETER_TYPE_H_
+#define HERCULES_CORE_PARAMETER_TYPE_H_
+
+#include "hercules/core/parameter_type.h"
+
+namespace hercule::core {
+
+    std::string_view to_string_view();
+}  // namespace hercule::core
+
+#endif  // HERCULES_CORE_PARAMETER_TYPE_H_
